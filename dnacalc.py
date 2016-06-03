e059c04c34705a601a4bc87ed640ba969882ede8
Gonzalo <maguszzz@hotmail.com>
2016-06-03 11:52:29 +0200

    With percentages

--- dnacalc.py
+++ dnacalc.py
@@ -5,8 +5,20 @@
 
 DNASeq = "ATGAAC"
 
-print ("Sequence " + DNASeq)
+print ( "Sequence " + DNASeq)
 
-SeqLength = len( DNASeq )
+SeqLength = float (len( DNASeq ))	#len cuenta las letras de una variable, es un integer
 
-print ( SeqLength )
+print ( "Length is " + str( SeqLength ) )	#str() dice que es un string lo que hay etre parentesis
+
+ # Si no ponemos str() el programa no funcionara pk no se pueden sumar strings e integers
+
+NumberA = DNASeq.count('A')
+NumberT = DNASeq.count("T")
+NumberG = DNASeq.count("G")
+NumberC = DNASeq.count("C")
+
+print ("A: " + str( (NumberA / SeqLength) *100 ) +"%" )
+print ("T: " + str( (NumberT / SeqLength) *100 ) +"%" )
+print ("G: " + str( (NumberG / SeqLength) *100 ) +"%" )
+print ("C: " + str( (NumberC / SeqLength) *100 ) +"%" )
