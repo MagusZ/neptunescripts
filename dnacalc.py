4d1c15dfd52d0be6cc4ff9a279f6cfd00d227489
Gonzalo <maguszzz@hotmail.com>
2016-06-03 11:12:27 +0200

    added DNAcalc

---
+++ dnacalc.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+# Ponemos env python para que lo encuentre en cualquier ordenador,
+# sea donde sea que este instalado. 
+
+DNASeq = "ATGAAC"
+
+print ("Sequence " + DNASeq)
+
